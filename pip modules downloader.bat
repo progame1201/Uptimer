0eb08ef03015e3c5bd05b528e169fe4274c0d1cf
progame1201 <108867463+progame1201@users.noreply.github.com>
2023-04-20 23:34:46 +0300

    Add files via upload

---
+++ pip modules downloader.bat
@@ -0,0 +1,8 @@
+pip install ping3
+pip install urllib
+pip install requests
+pip install configparser
+@echo off
+echo 
+echo all needs modules instaled
+pause
---
+++ pipmodulesdownloader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "pip modules downloader for linux"
+python3 -m pip install ping3
+python3 -m pip install urllib
+python3 -m pip install requests
+python3 -m pip install configparser
+exit 0
