a1b0f4b7f1ea395937fcd7118499338c32faa67a
progame1201 <108867463+progame1201@users.noreply.github.com>
2023-04-20 23:34:28 +0300

    Update Uptimer.py

--- Uptimer.py
+++ Uptimer.py
@@ -2,7 +2,49 @@
 import os
 from time import sleep
 import requests
+import configparser
+import os.path
+import json
+#config generation
+pathhere = os.path.exists(path="config.ini") #config here?
+config = configparser.ConfigParser()
+if pathhere == True : #regen start
+  errorread = False
+  try:
+    config.read("config.ini") #read
+  except AttributeError: # Attribute error?
+    print("")
+    errorread = True
+  if errorread == False : # read error?
+    configversion = config.get("Info", "version")
+    if configversion != "1.5.0" :
+     os.remove("config.ini")
+pathhere = os.path.exists(path="config.ini") #config here?
+#config generation end
+if pathhere == False : #if config not here
+ def createConfig(path): #conf create
+    config = configparser.ConfigParser()
+    config.add_section("Settings") # SETTINGS
+    config.set("Settings", "configtrue", "False") # CONF ENABLE?
+    config.set("Settings", "ip", "0.0.0.0") # IP
+    config.set("Settings", "port", "0000") # PORT for natcat
+    config.set("Settings", "method", "ask") #METHOD
+    config.set("Settings", "msgtype", "1") #msg type
+    config.set("Settings", "discordurl", "discord url here")  # discord url
+    config.set("Settings", "discordcont", "discord message here")  # discord msg
+    config.set("Settings", "API_URL", "api url")  # api url
+    config.set("Settings", "API_JSON", "api json")  # api json
+    config.set("Settings", "sleeptime", "30")  # sleep time
+    config.add_section("Info")
+    config.set("Info", "version", "1.5.0") # cfg version
+    with open(path, "w") as config_file:
+        config.write(config_file)
 
+ if __name__ == "__main__":
+     path = "config.ini"
+     createConfig(path)
+# config generation end
+configtrue = "False"
 result = 0
 port = 0
 sus = 0
@@ -11,8 +53,14 @@ UpTime = 0
 notonline = 0
 discordcont = "None"
 discordurl = "None"
+method = "nul"
+ip = "0.0.0.0"
+API_URL = "url"
+API_JSON = "json here"
 errcode = "Incorrect response from the host"
+readerror = False
 Checksuccesstimes = 0
+sleept = 30
 param = {
   "content": "Message from the user: " + discordcont,
   "embeds": [
@@ -24,44 +72,87 @@ param = {
   ],
   "attachments": []
 } # discord message
-print("Uptimer 1.4.0, build 8") #version and build
+pathhere = os.path.exists(path="config.ini")
+if pathhere == True :
+ try:
+  config.read("config.ini")
+ except AttributeError:
+  print("")
+  readerror = True
+
+ if readerror == False :
+   ip = config.get("Settings", "ip")
+   configtrue = config.get("Settings", "configtrue")
+   method = config.get("Settings", "method")
+   port = config.get("Settings", "port")
+   msgtype = config.get("Settings", "msgtype")
+   discordurl = config.get("Settings", "discordurl")
+   discordcont = config.get("Settings", "discordcont")
+   API_URL = config.get("Settings", "API_URL")
+   API_JSON = config.get("Settings", "API_JSON")
+   sleeptime = config.get("Settings", "sleeptime")
+   try:
+     sleept = int(sleeptime)
+   except ValueError:
+       print("")
+
+   try:
+    API_JSONR = json.loads(API_JSON)
+   except json.JSONDecodeError or json.JSONEncoderError :
+     print ("")
+
+
+if method == "ask" :
+ if configtrue == "True" :
+  print("methods:") #methods
+  print("ping - the easiest verification method !dont work with ports! (can be blocked by host)") # ping method
+  print("natcat - UDP/TCP verification method (work only on Unix systems)") # natcat method
+  print("request - HTTP/S verification method !dont work with IP and ports! (can be blocked by host)") # HTTP method
+  print("urllib - HTTP/S verification method !dont work with IP and ports! (can be blocked by host) needs instaled urllib3 module") # urllib3 method
+  print("ping3 - the method is similar to ping, but also compare ms with the set maximum. Needs installed ping3 module") # ping3 method
+  print("DEBUG: CONFIG READED")
+  method = input("method: ")
+
+print("Uptimer 1.5.0, build 24") #version and build
 print()
 print("Info") #info
 print("progame1201#8037 - general code writer")
-print("EVA#1130 - helping in scripting methods and fix errors")
-print("Rysik5318#7967 - help in the implementation of the idea, as well as in the method: request")
+print("EVA#1130 - helped with coding and bug fixing at first ")
+print("Rysik5318#7967 - helped in the implementation of the idea, as well as in tests with the urllib and request methods")
+print("moseechev#6235 - tester")
 sleep(1.5)
-print()
-print("methods:") #methods
-print("ping - the easiest verification method !dont work with ports! (can be blocked by host)") # ping method
-print("natcat - UDP/TCP verification method (work only on Unix systems)") # natcat method
-print("request - HTTP/S verification method !dont work with IP and ports! (can be blocked by host)") # HTTP method
-print("urllib - HTTP/S verification method !dont work with IP and ports! (can be blocked by host) needs instaled urllib3 module") # urllib3 method
-print("ping3 - the method is similar to ping, but also compare ms with the set maximum. Needs installed ping3 module") # ping3 method
-print()
-method = input("method: ")
-print()
-sleept = input("sleep time in sec. (use 30 or 240 to get the best result): ") #sleeptime
-print()
-print("message types: ")
-print("1 - discord webhook")
-print("2 - Using your API")
-print()
-msgtype = input("Type:")
-if msgtype == "1" :
- discordurl = input("discord webhook url: ") # discord webhook url setting
+if configtrue == "False" :
  print()
- discordcont = input("discord message on error: ") # discord message setting
+ print("methods:") #methods
+ print("ping - the easiest verification method !dont work with ports! (can be blocked by host)") # ping method
+ print("natcat - UDP/TCP verification method (work only on Unix systems)") # natcat method
+ print("request - HTTP/S verification method !dont work with IP and ports! (can be blocked by host)") # HTTP method
+ print("urllib - HTTP/S verification method !dont work with IP and ports! (can be blocked by host) needs instaled urllib3 module") # urllib3 method
+ print("ping3 - the method is similar to ping, but also compare ms with the set maximum. Needs installed ping3 module") # ping3 method
+ print()
+ method = input("method: ")
+ print()
+ sleept = input("sleep time in sec. (use 30 or 240 to get the best result): ") #sleeptime
+ print()
+ print("message types: ")
+ print("1 - discord webhook")
+ print("2 - Using your API")
+ print()
+ msgtype = input("Type: ")
+ if msgtype == "1" :
+  discordurl = input("discord webhook url: ") # discord webhook url setting
+  print()
+  discordcont = input("discord message on error: ") # discord message setting
 
-if msgtype == "2" :
- import json
- API_URL = input("API url: ") # API url
- API_JSON = input("json data: ") # API Json now it's string
- API_JSONR = json.loads(API_JSON) #now it's json type
+ if msgtype == "2" :
+  API_URL = input("API url: ") # API url
+  API_JSON = input("json data: ") # API Json now it's string
+  API_JSONR = json.loads(API_JSON) #now it's json type
 
 if method == "ping": # ping method start
- print()
- ip = input("host: ") # IP
+ if configtrue == "False" :
+  print()
+  ip = input("host: ") # IP
  while True:
    response = os.system("ping -w 2 " + ip) # pinging
    if response == 0:
@@ -115,11 +206,12 @@ if method == "ping": # ping method start
 if method == "natcat" : # natcat method start
   print("Warning! this method working only on unix systems!") #warn
   sleep(1)
-  print()
-  ip = input("host: ") # IP
-  print()
-  port = input("Port: ") #port
-  print()
+  if configtrue == "False":
+   print()
+   ip = input("host: ") # IP
+   print()
+   port = input("Port: ") #port
+   print()
   print("use udp - y / tcp - n")
   udptr = input("")
   while True :
@@ -178,7 +270,8 @@ if method == "natcat" : # natcat method start
     sleep(int(sleept)) # natcat method end
 
 if method == "request" : # HTTP method start
-  ru = input("URL: ")
+  if configtrue == "False":
+   ip = input("URL: ")
   print("requests types:")
   print("1 - HEAD")
   print("2 - POST")
@@ -186,11 +279,11 @@ if method == "request" : # HTTP method start
   rtype = input("Type: ") 
   while True :
     if rtype == "1" :
-     r = requests.head(ru) #head
+     r = requests.head(ip) #head
     if rtype == "2" :
-     r = requests.post(ru) #post
+     r = requests.post(ip) #post
     if rtype == "3" :
-     r = requests.get(ru) #get
+     r = requests.get(ip) #get
     status = r.status_code
     print(status)
 
@@ -246,7 +339,8 @@ if method == "request" : # HTTP method start
 
 if method == "urllib" : # urllib method start
    import urllib3
-   url = input("URL: ")
+   if configtrue == "False":
+    ip = input("URL: ")
    print("requests types:")
    print("1 - HEAD")
    print("2 - POST")
@@ -255,11 +349,11 @@ if method == "urllib" : # urllib method start
    while True :
      http = urllib3.PoolManager()
      if rtype == "1" :
-      i100 = http.request('HEAD', url) #head
+      i100 = http.request('HEAD', ip) #head
      if rtype == "2" :
-      i100 = http.request('POST', url) #post
+      i100 = http.request('POST', ip) #post
      if rtype == "3" :
-      i100 = http.request('GET', url) #get
+      i100 = http.request('GET', ip) #get
      status = i100.status
      print(i100.status)
      if status == 200 :
@@ -313,8 +407,8 @@ if method == "urllib" : # urllib method start
 
 if method == "ping3" : #ping3 method start
     from ping3 import ping, verbose_ping
-    ip = input("IP: ") #ip
-
+    if configtrue == "False":
+     ip = input("IP: ") #ip
     maxms = input("Max MS: ") #max ms
 
     while True :
