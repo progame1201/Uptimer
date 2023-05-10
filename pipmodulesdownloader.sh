3206bb34b98a34cb276c55292aacf9830a54afbc
progame1201 <108867463+progame1201@users.noreply.github.com>
2023-05-10 19:09:58 +0300

    Update pipmodulesdownloader.sh

--- pipmodulesdownloader.sh
+++ pipmodulesdownloader.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "pip modules downloader for linux"
 python3 -m pip install ping3
-python3 -m pip install urllib
+python3 -m pip install urllib3
 python3 -m pip install requests
 python3 -m pip install configparser
+python3 -m pip install psutil
 exit 0
